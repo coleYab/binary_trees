ee2883ae46d8b3cc2d4ebd775fa72e0125d74076
coleYab <yabume123@gamil.com>
2024-02-26 22:09:30 +0300

    added up to 12-*.c

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,16 +1,25 @@
 #include "binary_trees.h"
 
+size_t is_leaf(const binary_tree_t *node)
+{
+    return ((node->right || node->left) ? (0) : (1));
+}
+
+size_t count(const binary_tree_t *tree)
+{
+    size_t l_sum = 0, r_sum;
+    if (tree)
+    {
+        l_sum = (tree->left) ? (is_leaf(tree->left) + count(tree->left)) : (0);
+        r_sum = (tree->right) ? (is_leaf(tree->right) + count(tree->right)) : (0); 
+    }
+    return (l_sum + r_sum);
+}
+
+
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-    size_t l_right = 0, l_left = 0;
+    size_t total = count(tree);
 
-    if (!tree)
-        return (0);
-
-    if (tree->left || tree->right)
-    {
-        return (count())
-    }
-
-   return (1);
+    return ((total) ? (total) : (is_leaf(tree)));
 }
