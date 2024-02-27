265b9999fb685e1ba5c7278dd48b1f4c07fe8c12
coleYab <yabume123@gamil.com>
2024-02-27 11:56:20 +0300

    fixed betty

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,22 +1,32 @@
 #include "binary_trees.h"
 
+
+/**
+ * count - counts the size of the tree exept the node.
+ *
+ * @tree: the root of the tree.
+ *
+ * Return: the size of (tree) - 1.
+*/
 size_t count(const binary_tree_t *tree)
 {
-    size_t left = 0, right = 0;
+	size_t left = 0, right = 0;
 
-    left = (tree->left) ? (1 + count(tree->left)) : (0);
-    right = (tree->right) ? (1 + count(tree->right)) : (0);
+	left = (tree->left) ? (1 + count(tree->left)) : (0);
+	right = (tree->right) ? (1 + count(tree->right)) : (0);
 
-    return (tree) ? (left + right) : (0);
+	return (tree) ? (left + right) : (0);
 }
 
+
+/**
+ * binary_tree_size - is a function to count the size of a tree.
+ *
+ * @tree: root of a tree.
+ *
+ * Return: the size of tree.
+*/
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    int size = 1;
-
-    if (!tree)
-        return (0);
-    if (tree)
-        size += (int)count(tree);
-    return ((size_t)size);
+	return ((tree) ? (1 + count(tree)) : (0));
 }
