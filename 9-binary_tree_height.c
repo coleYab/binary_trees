ee4bc933ccf7fcc50865d34fe38347a83faeddb4
coleYab <yabume123@gamil.com>
2024-02-27 12:02:22 +0300

    fixed betty style

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,5 +1,12 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_height - a function to count the height of the tree.
+ *
+ * @tree: the tree to be checked.
+ *
+ * 
+*/
 size_t binary_tree_height(const binary_tree_t *tree)
 {
     size_t left_len = 0, right_len = 0;
