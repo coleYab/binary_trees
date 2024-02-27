8eeedf4cca0891f60d3abe81c5c3872b859d5390
coleYab <yabume123@gamil.com>
2024-02-27 11:44:18 +0300

    added up to 13-*.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -22,15 +22,15 @@ int is_not_leaf(const binary_tree_t *node)
 */
 size_t count(const binary_tree_t *tree)
 {
-	size_t l_nodes = 0, r_nodes = 0;
+	size_t l_no = 0, r_no = 0;
 
 	if (tree)
 	{
-		l_nodes = (tree->left) ? (is_not_leaf(tree->left) + count(tree->left)) : (0);
-		r_nodes = (tree->right) ? (is_not_leaf(tree->right) + count(tree->right)) : (0);
+		l_no = (tree->left) ? (is_not_leaf(tree->left) + count(tree->left)) : (0);
+		r_no = (tree->right) ? (is_not_leaf(tree->right) + count(tree->right)) : (0);
 	}
 
-	return (l_nodes + r_nodes);
+	return (l_no + r_no);
 }
 
 /**
