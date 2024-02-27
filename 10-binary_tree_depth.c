ed9e886b027f19f73ee1d7ae5df4c87849de2646
coleYab <yabume123@gamil.com>
2024-02-27 12:00:15 +0300

    fixed betty style

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,8 +1,13 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_depth - counts the depth of a binary tree.
+ *
+ * @tree: the tree to be counted.
+ *
+ * Return: the depth of a root from the tree node.
+*/
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-    if (!tree->parent)
-        return (0);
-    return (1 + binary_tree_depth(tree->parent));
+	return ((tree->parent) ? (1 + binary_tree_depth(tree->parent)) : (0));
 }
